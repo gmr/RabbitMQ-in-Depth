4b82d4478713882f20f169f91a65d04f7e26c244
Gavin M. Roy <gavinmroy@gmail.com>
2013-09-30 14:28:52 -0400

    Fix some bugs

--- notebooks/ch6_facedetect.py
+++ notebooks/ch6_facedetect.py
@@ -10,14 +10,15 @@ def boxes(filename, faces):
         return
     img = cv2.imread(filename)
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (127, 255, 0), 1)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (127, 255, 0), 2)
+    filename = filename.split('/')[-1]
     parts = filename.split('.')
-    new_name = '%s-detected.%s' % (parts[0], parts[1])
-    cv2.imwrite('/tmp/%s' % new_name, img)
-    return '/tmp/%s' % new_name
+    new_name = '/tmp/%s-detected.%s' % (parts[0], parts[1])
+    cv2.imwrite(new_name)
+    return new_name
 
 
 def detect(filename, ratio=1.3, neighbors=4, min_size=(10, 10)):
     img = cv2.imread(filename, 0)
     hc = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    return boxes(filename, hc.detectMultiScale(img, ratio, neighbors, cv2.cv.HAAR_SCALE_IMAGE, min_size))
+    return boxes(filename, hc.detectMultiScale(img, ratio, neighbors, cv2.cv.CV_HAAR_SCALE_IMAGE, min_size))
